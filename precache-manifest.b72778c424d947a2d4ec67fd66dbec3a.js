1c58fc5ec94ef8d3bb723d9a3d9c6c13791bc6c4
var-greyShader <yasharyan307@outlook.com>
2020-07-19 22:14:58 +0530

    Updates

---
+++ precache-manifest.b72778c424d947a2d4ec67fd66dbec3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7394d92b1d8098e15805aaaabafd62f3",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "3633f1146d67df73c9d8",
+    "url": "/todoApp/static/css/main.8b546833.chunk.css"
+  },
+  {
+    "revision": "a0b47887dbe77b4c002b",
+    "url": "/todoApp/static/js/2.ef5c8690.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.ef5c8690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3633f1146d67df73c9d8",
+    "url": "/todoApp/static/js/main.22de7103.chunk.js"
+  },
+  {
+    "revision": "ce3ef7ffb8d09fa34afe",
+    "url": "/todoApp/static/js/runtime-main.a7bb4c2a.js"
+  },
+  {
+    "revision": "c0242722f9a540c43391af421810f9a0",
+    "url": "/todoApp/static/media/mountains.c0242722.jpg"
+  }
+]);
